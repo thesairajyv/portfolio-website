d74666d4ce07b513c41d2cceeb4113ec62af2268
“thesairajyv” <learner4me4mine@gmail.com>
2025-04-01 01:09:01 +0530

    Update Vite config for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/sairajyv/',
+  base: '/portfolia-website/',
   build: {
     outDir: 'dist',
     sourcemap: false,
